60983342239b3471056f2268f128dd993b1a88c9
tiebanluyu <2708702597@qq.com>
2024-01-05 12:17:30 +0800

    adapt new veision pyinstaller

--- level3.py
+++ level3.py
@@ -12,23 +12,25 @@ def level3(code: str):
         f.write(code)
 
     # 执行pyinstaller
-    logging.info("可能pyinstaller打包的时间有点长，请稍等")
+
     logging.info("Please wait.")
-    os.system("cd dist&& pyinstaller  code.py")
+    os.system("cd dist&& pyinstaller code.py")
 
 
-    exceptions = ["code.exe", "base_library.zip"]
-    for filename in os.listdir("dist/dist/code"):
-        if filename.startswith("python"):
-            exceptions.append(filename)
 
+    
     # 删除文件和移动文件
     shutil.copytree("dist/dist/code", "dist_temp")
-    # shutil.rmtree("dist")#按理说可以删除非空文件夹，可就是不行，用cmd命令
-    os.system("rmdir /s /q dist")
+    shutil.rmtree("dist")#按理说可以删除非空文件夹，可就是不行，用cmd命令
+    #os.system("rmdir /s /q dist")
 
     shutil.move("dist_temp", "dist")
-    delete_files_except("dist", exceptions)
+    
+    exceptions = ["code.exe", "base_library.zip"]
+    for filename in os.listdir("dist/_internal"):
+        if filename.startswith("python"):
+            exceptions.append(filename)
+    delete_files_except("dist/_internal", exceptions)
 
 
 def delete_files_except(des, exceptions):
